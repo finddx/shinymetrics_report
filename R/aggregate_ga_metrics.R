b747e5018d46eda8ae1bb9815a77142a033f5df5
findanna <anna.mantsoki@finddx.org>
2024-06-13 15:16:18 +0200

    update ga analytics data
    - add data from 2023 and 2024
    - add mpoc and cxr testdirs

--- R/aggregate_ga_metrics.R
+++ R/aggregate_ga_metrics.R
@@ -21,13 +21,17 @@ ga_metrics_df_weekly_2023 <- read_csv(files_2023, id="path") |>
   mutate(date = lubridate::as_date(date, format = "%d %m %Y")) |>
   mutate(application = stringr::str_replace(path, "data/google_analytics/20231107 - 2023 Pages views for test directories_", ""))
   
-  
-files_2024 <- fs::dir_ls("data/google_analytics/", glob="* - 2024*.csv")
-ga_metrics_df_weekly_2024 <- read_csv(files_2024, id="path") |>
-  select(path, date = Date, page_views = Views) |>
-  mutate(date = lubridate::as_date(date, format = "%d %m %Y")) |>
-  mutate(application = stringr::str_replace(path, "data/google_analytics/20240611 - 2024 Pages views for test directories_", ""))
 
+files_2024 <- fs::dir_ls("data/google_analytics/", glob="* - 2024*.csv")
+
+ga_metrics_df_weekly_2024 <- map_dfr(files_2024, ~ {
+  read_csv(.x, id = "path")|>
+    select(path, date = Date, page_views = Views) |>
+    mutate(
+      date = as_date(date, format = "%d %m %Y"),
+      application = str_replace(.x, "data/google_analytics/20240611 - 2024 Pages views for test directories_", "")
+    )
+})
 
 
 ga_metrics_df_weekly <- bind_rows(ga_metrics_df_weekly_2020_2022, ga_metrics_df_weekly_2023, ga_metrics_df_weekly_2024) |>
@@ -45,7 +49,9 @@ ga_metrics_df_weekly <- bind_rows(ga_metrics_df_weekly_2020_2022, ga_metrics_df_
                                  application == "COVID-19 test directory_Table" ~ "FIND Test directory (COVID-19)",
                                  application == "Outbreaks test directory_Table" ~ "FIND Test directory (Outbreaks)",
                                  application == "TB test directory_Table" ~ "FIND Test directory (TB)",
-                                 application == "NTDs test directory_Table" ~ "FIND Test directory (NTDs)"
+                                 application == "NTDs test directory_Table" ~ "FIND Test directory (NTDs)",
+                                 application == "CXR test directory_Table" ~ "FIND Test directory (CXRs)",
+                                 application == "Molecular POC test directory_Table" ~ "FIND Test directory (Molecular POC)"
   )
   ) |>
   filter(!is.na(date)) |>
